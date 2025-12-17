f0b76e3e3923e074fb82c1e423262486e7fad40a
supracodespace <komatinenisupraja@gmail.com>
2025-12-17 21:42:54 +0800

    updated webapp

--- src/main/java/com/ns/services/GetPlayerDetails.java
+++ src/main/java/com/ns/services/GetPlayerDetails.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/employee")
+@RequestMapping("/player")
 public class getPlayerDetails {
 
 
